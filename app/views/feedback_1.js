a826a0bbead945ac49d8254172c77fed27380176
Alex Colbeck <alexcolbeck@me.com>
2017-10-30 16:19:22 +0000

    Updating logic to account for checkboxes

--- app/views/feedback_1.js
+++ app/views/feedback_1.js
@@ -1,14 +1,30 @@
-module.exports = function (input, req) {
-  switch (input.action) {
-    case 'staff':
-      input.redirect = '/feedback_1_1';
-      break;
-    case 'public':
-      input.redirect = '/feedback_1_2';
-      break;
-    case 'investigator':
-      input.redirect = '/feedback_1_3';
-      break;
+module.exports = function(input, req) {
+  if (!Array.isArray(input.action)) {
+    console.log(input.action);
+    switch (input.action) {
+      case 'staff':
+        input.redirect = '/feedback_1_1';
+        break;
+      case 'public':
+        input.redirect = '/feedback_1_2';
+        break;
+      case 'investigator':
+        input.redirect = '/feedback_1_3';
+        break;
+    }
+  } else {
+    if (input.action.length === 2) {
+      if (input.action[0]=== 'staff' && input.action[1]=== 'public') {
+        input.redirect = '/feedback_1_4';
+      } else if (input.action[0]=== 'staff' && input.action[1]=== 'investigator') {
+        input.redirect = '/feedback_1_5';
+      } else if (input.action[0]=== 'public' && input.action[1]=== 'investigator') {
+        input.redirect = '/feedback_1_6';
+      }
+    } else {
+      // This section is for all options selected.
+      input.redirect = '/feedback_1_7';
+    }
   }
 
   return input;
