b85ab8f96b1ca75c96afd0c05d28723d916de315
Alex Colbeck <alexcolbeck@me.com>
2017-10-30 15:35:33 +0000

    Updating names with logic

--- app/views/feedback_1.js
+++ app/views/feedback_1.js
@@ -1,21 +1,14 @@
 module.exports = function (input, req) {
   switch (input.action) {
-    case 'address-my-issue':
+    case 'staff':
       input.redirect = '/feedback_1_1';
       break;
-    case 'influence-others':
+    case 'public':
       input.redirect = '/feedback_1_2';
       break;
-    case 'improve-services':
+    case 'investigator':
       input.redirect = '/feedback_1_3';
       break;
-    case 'nhs-investigate':
-      input.redirect = '/feedback_1_4';
-      break;
-    case 'say-something-positive':
-      input.redirect = '/feedback_1_3';
-      break;
-
   }
 
   return input;
