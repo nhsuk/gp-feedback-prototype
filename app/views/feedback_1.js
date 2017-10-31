2898448700a3b916fdf918949fb870b5d8226f73
Alex Colbeck <alexcolbeck@me.com>
2017-10-31 21:29:47 +0000

    Creating a step in the feedback journey, that asks the user `What do you want the outcome to be?`

--- app/views/feedback_1.js
+++ app/views/feedback_1.js
@@ -3,27 +3,27 @@ module.exports = function(input, req) {
     console.log(input.action);
     switch (input.action) {
       case 'staff':
-        input.redirect = '/feedback_1_1';
+        input.redirect = '/feedback_1_outcome_1';
         break;
       case 'public':
-        input.redirect = '/feedback_1_2';
+        input.redirect = '/feedback_1_outcome_2';
         break;
       case 'investigator':
-        input.redirect = '/feedback_1_3';
+        input.redirect = '/feedback_1_outcome_3';
         break;
     }
   } else {
     if (input.action.length === 2) {
       if (input.action[0]=== 'staff' && input.action[1]=== 'public') {
-        input.redirect = '/feedback_1_4';
+        input.redirect = '/feedback_1_outcome_4';
       } else if (input.action[0]=== 'staff' && input.action[1]=== 'investigator') {
-        input.redirect = '/feedback_1_5';
+        input.redirect = '/feedback_1_outcome_5';
       } else if (input.action[0]=== 'public' && input.action[1]=== 'investigator') {
-        input.redirect = '/feedback_1_6';
+        input.redirect = '/feedback_1_outcome_6';
       }
     } else {
       // This section is for all options selected.
-      input.redirect = '/feedback_1_7';
+      input.redirect = '/feedback_1_outcome_7';
     }
   }
 
