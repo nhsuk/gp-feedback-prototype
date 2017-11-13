f3c53c418efbdc49daf17696dec28e9f6ff14d8b
Alex Colbeck <alexcolbeck@me.com>
2017-11-13 10:52:45 +0000

    Updating logic for complaints journey

---
+++ app/views/7a_who_did_you_complain_to.js
@@ -0,0 +1,21 @@
+module.exports = function(input, req) {
+  switch (input.action) {
+    case 'gp':
+      input.redirect = '/8a_contact_phso';
+      break;
+    case 'nhs-england':
+      input.redirect = '/8b_contact_phso';
+      break;
+    case 'phso':
+      input.redirect = '/8c_why_arent_you_happy_with_phso';
+      break;
+    case 'gmc':
+      input.redirect = '/8d_ask_for_judicial_review';
+      break;
+    case 'nmc':
+      input.redirect = '/8e_ask_for_judicial_review';
+      break;
+  }
+
+  return input;
+}
