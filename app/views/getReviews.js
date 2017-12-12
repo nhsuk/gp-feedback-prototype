eebe8e2a1fbcbcc22af698382f01ad00b408045e
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-12-12 01:05:27 +0000

    Add surgery reponses

--- app/views/getReviews.js
+++ app/views/getReviews.js
@@ -3,16 +3,41 @@ const fs = require('fs');
 
 module.exports = (function () {
 
-  const xml = fs.readFileSync('app/data/reviews.xml', "utf8");
-  var json = parser.toJson(xml, { object: true });
-  return json.feed.entry.filter(e => e.content.div).map(e => { 
-    // console.log(e);
-    return {
-      title: e.title["$t"],
-      displayname: e.author.name,
-      body: Array.isArray(e.content.div) ? e.content.div[0]['$t'] : e.content.div['$t'],
-      monthOfVisit: e.commentattribution['$t']
-    }
+  let xml = fs.readFileSync(`app/data/reviews-p1.xml`, "utf8");
+  let json = parser.toJson(xml, { object: true });
+  allComments = json.feed.entry;
+
+  const reviews = allComments
+    .filter(e => e.category.term === 'comment' && e.content.div)
+    .map(e => { 
+      return {
+        id: e.id,
+        title: e.title["$t"],
+        displayname: e.author.name,
+        body: Array.isArray(e.content.div) ? e.content.div[0]['$t'] : e.content.div['$t'],
+        monthOfVisit: e.commentattribution['$t']
+      }
   });
+
+  const responses = allComments
+    .filter(e => e.category.term === 'reply')
+    .map(e => { 
+      return {
+        inReplyTo: e['in-reply-to'].ref,
+        body: e.content['$t'],
+        monthOfVisit: e.commentattribution['$t']
+      }
+  });
+
+  return reviews.map(review => {
+    const response = responses.find(response => response.inReplyTo === review.id);
+    if (response) {
+      review.response = response.body;
+    };
+
+    return review;
+  });
+
+  return reviews;
 })();
 
