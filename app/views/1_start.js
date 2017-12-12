4918dea1eae68fa15dfdcfdb2004fa454c48d1f4
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-12-12 15:28:29 +0000

    Control which review screen is presented using an env var

--- app/views/1_start.js
+++ app/views/1_start.js
@@ -2,9 +2,11 @@ const reviews = require('./getReviews');
 
 module.exports = function (input, req) {
   if (req.method === 'POST') {
+    const version = process.env.VERSION || 'v1';
     req.session.validated = {};
     req.session.validated = Object.assign({}, req.session.validated, {
-      reviews: reviews
+      reviews: reviews,
+      version: version
     })
 
     input.redirect = '4_when_did_this_happen';
